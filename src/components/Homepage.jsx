5541a6ad63de2f7e5638a99c58135ac96c141887
MackenzLaptop <youzai0427@gmail.com>
2021-11-10 02:27:38 +0800

    func✅ contact-btn

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import avatar from "../assets/avatar.webp";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 
 export default function Homepage() {
   return (
@@ -7,10 +9,14 @@ export default function Homepage() {
       <div className="self-introduce">
         <h1>Introduce myself</h1>
         <p>
-          Hi I’m Mack from Malaysia and I’m interested in web and mobile
-          Development. I’m also a little interested about 3D stuff and animation
+          Hi I’m <span>Mack</span> from Malaysia. I’m a Front-end and Mobile
+          Developer. I’m also interested in 3D stuff and animation
         </p>
-        <button>Contact me</button>
+        <Button className="contact-btn" variant="contained">
+          <Link className="contact-btn-link" to="/contact">
+            Contact me
+          </Link>
+        </Button>
       </div>
       <div className="image-wrapper">
         <img src={avatar} alt="this is a seal and i love it" />
