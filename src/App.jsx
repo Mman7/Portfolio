a39de1a7454627589aa0b27e8a71b59c36a464c6
MackenzLaptop <youzai0427@gmail.com>
2021-11-10 02:26:35 +0800

    Add➕react lazy import + react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,49 @@
-import { useState } from "react";
+import React, { Suspense } from "react";
 import "./App.scss";
+
 import BackgroundText from "./components/BackgroundText";
-import Homepage from "./components/Homepage";
+const Homepage = React.lazy(() => import("./components/Homepage"));
+const Project = React.lazy(() => import("./components/Project"));
+const Contact = React.lazy(() => import("./components/Contact"));
+import { Routes, Route, Link, useLocation } from "react-router-dom";
+
+import KeyboardArrowRightSharpIcon from "@mui/icons-material/KeyboardArrowRightSharp";
+import KeyboardArrowLeftSharpIcon from "@mui/icons-material/KeyboardArrowLeft";
 
 function App() {
+  const location = useLocation();
+  const path = ["/", "/project", "/contact"];
+  const getIndex = () => path.indexOf(location.pathname);
+
+  const nextPage = () => {
+    const index = getIndex();
+    const nextPath = path[index + 1];
+    return nextPath;
+  };
+  const previousPage = () => {
+    const index = getIndex();
+    const prevPath = path[index - 1];
+    return prevPath;
+  };
+
   return (
     <div className="App">
       <BackgroundText />
-      <Homepage />
+      <Suspense fallback={<div>...</div>}>
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Suspense>
+      <Link to={{ pathname: nextPage() }}>
+        <KeyboardArrowRightSharpIcon className="arrow-right-icon icon"></KeyboardArrowRightSharpIcon>
+      </Link>
+      {location.pathname !== "/" && (
+        <Link to={{ pathname: previousPage() }}>
+          <KeyboardArrowLeftSharpIcon className="arrow-left-icon icon"></KeyboardArrowLeftSharpIcon>
+        </Link>
+      )}
     </div>
   );
 }
