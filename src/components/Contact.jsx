ff12045f55ca876b721598507f5f3f8b9d2f8758
MackenzLaptop <youzai0427@gmail.com>
2021-11-10 02:27:15 +0800

    Add📁 new components

---
+++ src/components/Contact.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div className="contact">
+      <h1>THIS IS CONTACT ME</h1>
+    </div>
+  );
+}
---
+++ src/components/Project.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Project() {
+  return (
+    <div className="project">
+      <h1>THIS IS PROJECT</h1>
+    </div>
+  );
+}
